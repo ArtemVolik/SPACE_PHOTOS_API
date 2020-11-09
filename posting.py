2fe894bcdc008e54641c787d8ddf664df3aef13a
artyom <art.volyk@gmail.com>
2020-11-09 16:50:22 +0200

    hide in main()

--- posting.py
+++ posting.py
@@ -7,6 +7,7 @@ import logging
 from space_api_func import create_dir
 logger = logging.getLogger(__file__)
 
+
 def transform_image(picture):
     image = Image.open(picture)
     if image.mode != 'RGB':
@@ -25,6 +26,9 @@ def post_photo(photo, insta_login, insta_password):
 
 def main():
     load_dotenv()
+    logging.basicConfig(level=logging.ERROR)
+    logger.setLevel(logging.DEBUG)
+    image_folder = create_dir()
     insta_login = os.getenv('INSTA_LOGIN')
     insta_password = os.getenv('INSTA_PASSWORD')
     for image in os.listdir(image_folder):
@@ -35,9 +39,6 @@ def main():
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.ERROR)
-    logger.setLevel(logging.DEBUG)
-    image_folder = create_dir()
     main()
 
 
