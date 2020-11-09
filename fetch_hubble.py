a034c059c0b7107194788ba6206bbd8e1f775392
artyom <art.volyk@gmail.com>
2020-11-09 16:47:48 +0200

    add explicit folder variable to func params

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -3,7 +3,7 @@ import os
 from space_api_func import get_response, create_dir
 
 
-def fetch_habble(id):
+def fetch_habble(id, folder):
     links = get_response(f'http://hubblesite.org/api/v3/image/{id}')
     picture_link = links['image_files'][-1]['file_url']
     response = requests.get(f'http:{picture_link}')
@@ -12,15 +12,15 @@ def fetch_habble(id):
         file.write(response.content)
 
 
-def fetch_habble_collection(habble_collection='holiday_cards'):
+def fetch_habble_collection(folder, habble_collection='holiday_cards'):
     collection = get_response(f'http://hubblesite.org/api/v3/images/{habble_collection}')
     collection_ids = [photo['id'] for photo in collection]
     print(f'Saving {len(collection_ids)} photos from Hubble')
     for number, id in enumerate(collection_ids, 1):
-        fetch_habble(id)
+        fetch_habble(id, folder)
         print('Photo id:', id, 'saved.')
 
 
 if __name__ == '__main__':
     folder = create_dir()
-    fetch_habble_collection()
+    fetch_habble_collection(folder)
