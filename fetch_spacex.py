b6a23c7a3a110383b8bae0708b865ed5ab1f2fd0
artyom <art.volyk@gmail.com>
2020-11-09 03:39:21 +0200

    remove redundant func

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -8,32 +8,14 @@ urls = {
 }
 
 
-def search_images_array_in_json(search, search_key='flickr', pictures_found=[]):
-    for key, value in search.items():
-        if key == search_key:
-            pictures_found.append(value)
-            return
-        elif isinstance(value, dict):
-            search_images_array_in_json(value, search_key, pictures_found)
-        elif isinstance(value, list):
-            for item in value:
-                if isinstance(item, dict):
-                    search_images_array_in_json(item, search_key, pictures_found)
-    return pictures_found
-
-
 def get_space_x_images_list():
     last_flight = get_response(urls['latest'])
-    pictures_found = search_images_array_in_json(last_flight, search_key='flickr')[0]['original']
-    if pictures_found:
-        return pictures_found
+    if last_flight['links']['flickr']['original']:
+        return last_flight['links']['flickr']['original']
     else:
         flights = get_response(urls['all'])
-        for flight in flights:
-            pictures_found_from_all = search_images_array_in_json(flight, search_key='flickr', pictures_found=[])[0][
-                'original']
-            if pictures_found_from_all:
-                return pictures_found_from_all
+        flight_images = [flight['links']['flickr']['original'] for flight in flights if flight['links']['flickr']['original']]
+        return flight_images[1]
 
 
 def save_spacex_images(images, folder):
